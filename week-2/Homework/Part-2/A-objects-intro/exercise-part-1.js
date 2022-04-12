6b8c6fa4bbc03dc4419c598ef59e3c1420eb8f35
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-12 21:42:17 +0200

    adding exercise 1 - A

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
@@ -17,3 +17,59 @@ var car = {
 
 */
 
+const house = {
+    country: "Brazil",
+    state: "Sao Paulo",
+    totalArea: "170m",
+    numberOfRooms: 9,
+    numberofBathrooms:4,
+    isTherePool: true,
+    isThereThrees: true,
+    whoLivesThere: "mother",
+}
+
+const personalInfo = {
+    height = 1,65,
+    weight = 72,
+    country = "Brazil",
+    hobbie = "dance and paint",
+    likeNormalSports = false,
+    exercises = "yoga and dance",
+    art = "paint and dance",
+    favClimate = "spring and fall",
+    driveLicense: "car",
+    vacations = "travels",
+    kindOfFood: "sweets",
+    number: "even",
+    
+}
+
+
+const travel = {
+    next = "Portugal",
+    last = "Denmark",
+    favoriteBrazil = "Vrinda Bhumi",
+    favoriteEurope = "Malta",
+    dream = "Amazonia",
+    worst = 0,
+
+}
+const routine = {
+    Sunday = "study all day long",
+    Monday = "CodeBuddy Manana",
+    Tuesday = "Terapist Luciana",
+    Wednesday = "MigraCode Online",
+    Thursday = "Yoga on the break of work"
+    Friday = "Aero Yoga" ,
+    Saturday = "MigraCode on CaixaTeam"
+}
+
+const birthDays = {
+    Mom = 19/1,
+    Dad = 09/09,
+    Brother = 04/08,
+    Niece = 26/05,
+    Felix = 15/3,
+    Me = 16/11
+    GrandPa = 09/01,
+}
