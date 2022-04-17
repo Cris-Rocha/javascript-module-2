863e11633760be502f144e3a626f092411e82adb
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-17 14:04:47 +0200

    refactoring the exercise part 1 and part 2 with a codebuddy for improvement of the code

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -11,6 +11,49 @@ const orangeBtn = document.querySelector("#orangeBtn");
 const greenBtn = document.querySelector("#greenBtn");
 
 // Change color when clicking the button
+
+// exercise made by me:
+
+/*
+
+const clickBlueBtn = () => {
+  changingJumbotron.style.backgroundColor = "588fbd";
+  donateBtn.style.backgroundColor = "#ffa500";
+  volunteerBtn.style.backgroundColor = "black";
+  volunteerBtn.style.color = "white";
+};
+
+blueBtn.addEventListener("click", clickBlueBtn);
+
+// - - When clicking **orange** it should change:
+
+const orangeBtn = document.querySelector("#orangeBtn");
+
+const clickOrangeBtn = () => {
+  changingJumbotron.style.backgroundColor = "f0ad4e";
+  donateBtn.style.backgroundColor = "#5751fd";
+  volunteerBtn.style.backgroundColor = "31b0d5";
+  volunteerBtn.style.color = "white";
+};
+
+orangeBtn.addEventListener("click", clickOrangeBtn);
+
+// - When clicking **green** it should change:
+
+const greenBtn = document.querySelector("#greenBtn");
+
+const clickGreenBtn = () => {
+  changingJumbotron.style.backgroundColor = "87ca8a";
+  donateBtn.style.backgroundColor = "#black";
+  volunteerBtn.style.backgroundColor = "8c9c08";
+};
+
+greenBtn.addEventListener("click", clickGreenBtn);
+
+*/
+
+// exercise part 1 refactored with the codebuddy:
+
 const changeColorStyleClickBtn = (
   jambotronBgColor,
   donateBgColor,
@@ -82,7 +125,7 @@ submitBtn.addEventListener("click", checkValidFields);
 
 */
 
-/// exercise refactored with the codebuddy:
+/// exercise part 2 refactored with the codebuddy:
 
 const emailField = document.querySelector("#exampleInputEmail1");
 const yourNameField = document.querySelector("#example-text-input");
